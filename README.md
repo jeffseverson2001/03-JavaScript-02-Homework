8071f8242592ac496867194d72d010d34e2afaa7
jeffseverson2001 <jeff@zealouscreations.com>
2021-05-01 12:40:27 -0500

    Final Project

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # 03-JavaScript-02-Homework
-Week 3 Homework
+
+## Description
+
+Generates a password with a lenght between 8 and 128 characters long with the choice of 4 different types of characters.
+
+- To really understand the basic javascript commands.
+- It creates passwords that can be used in real situations.
+
+
+##  Useage
+
+- Click on the Generate Password Button.
+- Enter in the lenght of the password you would like.
+- You will be asked if you want to include each of the four character sets.
+    - Uppercase
+    - Lowercase
+    - Numaric
+    - Special
+- Finished password will show in box on webpage.
+
+Published Github Site URL:
+https://jeffseverson2001.github.io/03-JavaScript-02-Homework/
+
+    ```md
+    ![alt Password Generator](/assets/03-javascript-homework-demo.png)
+    ```
+
+##  Credits
+
+University of Minnesota Boot Camp:
+https://umn.bootcampcontent.com/University-of-Minnesota-Boot-Camp
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,12 @@ function writePassword() {
 
 
 function generatePassword() {
-  // alert("Hello");
-
   let password = "";
+  let allParts = [];
 
   let totalLength = window.prompt("How many characters do you want for your password?");
   if (totalLength < 8 || totalLength > 128 || totalLength === "" || isNaN(totalLength)) {
-    alert("You must choose between 8 and 128 characters for your password.\nTry Again.");
+    alert("You MUST choose between 8 and 128 characters for your password.\nTry Again.");
     return password;
   }
 
@@ -31,23 +30,12 @@ function generatePassword() {
   let useNumber = window.confirm("Would you like to include Numeric characters?");
   let useSpecial = window.confirm("Would you like to include Special characters?");
 
+  if (useUpper === false && useLower === false && useNumber === false && useSpecial === false) {
+    alert("You MUST select at least one character type.\nTry Again.");
+    return password;
+  }
+
   console.log(totalLength);
-
-  let partsArray = [totalLength, useUpper, useLower, useNumber, useSpecial];
-
-  console.log(partsArray);
-
-  password = createPassword(partsArray);
-  return password;
-}
-
-
-
-function createPassword(criteria) {
-
-  let allParts = [];
-  let indexList = [];
-  let passwordString = [];
 
   //  Set Password Part Strings
   let part1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -55,106 +43,46 @@ function createPassword(criteria) {
   let part3 = "01234567890123456789";
   let part4 = "!\"#$%& '()*+,-./:;<=>?@[\]^_`{|}~";
 
+  //  Get even number of charaters selected
   let partDivider = 0;
+  if (useUpper) { partDivider++; }
+  if (useLower) { partDivider++; }
+  if (useNumber) { partDivider++; }
+  if (useSpecial) { partDivider++; }
 
-  if (criteria[1]) {
-    partDivider++;
-  }
-  if (criteria[2]) {
-    partDivider++;
-  }
-  if (criteria[3]) {
-    partDivider++;
-  }
-  if (criteria[4]) {
-    partDivider++;
-  }
-
-
-  let totalParts = criteria[0] / partDivider;
-  // let totalPartsDivider = Math.floor(totalParts);
-
+  let totalParts = totalLength / partDivider;
 
   for (let j = 0; j < totalParts; j++) {
-    if (criteria[1]) {
+    if (useUpper) {
       allParts.push(part1.charAt(Math.floor(Math.random() * part1.length)));
     }
-    if (criteria[2]) {
+    if (useLower) {
       allParts.push(part2.charAt(Math.floor(Math.random() * part2.length)));
     }
-    if (criteria[3]) {
+    if (useNumber) {
       allParts.push(part3.charAt(Math.floor(Math.random() * part3.length)));
     }
-    if (criteria[4]) {
+    if (useSpecial) {
       allParts.push(part4.charAt(Math.floor(Math.random() * part4.length)));
     }
 
   }
 
-  console.log(allParts);
-
+  //  Shuffle the order all characters
   let partsLength = allParts.length;
+  let shuffledParts = allParts.sort(() => Math.random() - 0.5);
 
-  if (criteria[0] > partsLength) {
+  //  Convert Array to String
+  let stringParts = shuffledParts.join("");
+
+  //  Size String to requested character length
+  if (totalLength > partsLength) {
     var useParts = partsLength;
   } else {
-    var useParts = criteria[0];
+    var useParts = totalLength;
   }
 
-  for (let i = 0; i < useParts; i++) {
-    indexList.push(i).charAt;
-  }
-
-  console.log(indexList);
-
-  let shuffled = indexList.sort(() => Math.random() - 0.5);
-
-  console.log(shuffled);
-
-  for (let i = 0; i < useParts; i++) {
-    passwordString = passwordString.concat(allParts.shuffled);
-  }
-
-  console.log(passwordString);
-
-  //var x = fruits.toString();
-
-
-  /*
-  let shuffleParts = "";
-  let checkIndex = 0;
- 
-  if (checkIndex === ) {
- 
-  }
-  Math.floor(Math.random() * criteria[0]) + 1;
-*/
-  //console.log("After: " + finalPass);
-
-
-  /*
-  if (criteria[1]) {
-    for (var i = 0; i < totalPartsDivider; i++) {
-      upperParts.push(part1.charAt(Math.floor(Math.random() *
-        part1.length)));
-    }
-  }
-  */
-
-
-  let randomPart1 = Math.random() * criteria[0];
-  let randomPart2 = Math.floor(Math.random() * criteria[0]) + 1;
-  console.log("Rand1: " + randomPart1);
-  console.log("Rand2: " + randomPart2);
-  /*
-    for (var i = 0; i < length; i++) {
-      result.push(characters.charAt(Math.floor(Math.random() *
-        charactersLength)));
-    }
-    return result.join('');
-    */
-
-  let password = "TheBirdIsTheWord";
+  password = stringParts.substring(0, useParts);
 
   return password;
 }
